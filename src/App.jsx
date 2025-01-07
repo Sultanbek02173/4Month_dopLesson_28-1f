ac4f244f4637390f229f9b1dd5ee0faac40d3638
Sultanbek02173 <sultan.02173@gmail.com>
2025-01-07 18:46:24 +0600

    create calculator

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import './App.css'
+
+function App() {
+
+  const [num1, setNum1] = useState();
+  const [num2, setNum2] = useState();
+  const [result, setResult] = useState();
+
+  return (
+    <>
+      <div>
+        <input onChange={(e) => {setNum1(+e.target.value)}} type="text" />
+        <input onChange={(e) => {setNum2(+e.target.value)}} type="text" />
+      </div>
+
+      <div>
+        <button onClick={() => setResult(num1 + num2)}>+</button>
+        <button onClick={() => setResult(num1 - num2)}>-</button>
+        <button onClick={() => setResult(num1 * num2)}>*</button>
+        <button onClick={() => setResult(num1 / num2)}>/</button>
+      </div>
+      <h1>Result:</h1>
+      <h2>{result}</h2>
+    </>
+  )
+}
+
+export default App
